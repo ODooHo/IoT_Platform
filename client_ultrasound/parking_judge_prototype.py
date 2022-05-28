3324208aadc245bbfdf2b0990b6a81f07d4566e1
Ohdooho <engh0205@naver.com>
2022-05-28 18:06:40 +0100

    IoT

--- client_ultrasound/parking_judge_prototype.py
+++ client_ultrasound/parking_judge_prototype.py
@@ -4,9 +4,27 @@ import matplotlib.pyplot as plt
 import pytesseract
 import requests
 import urllib
+import RPi.GPIO as GPIO
+import time
 from urllib.parse import quote_plus
 from urllib.parse import unquote_plus
 
+GPIO.setmode(GPIO.BCM)
+# 초음파
+A_TRIG = 23 
+A_ECHO = 24
+
+# LED
+A_RED = 17
+A_GREEN = 18
+
+GPIO.setup(A_TRIG, GPIO.OUT)
+GPIO.setup(A_ECHO, GPIO.IN)
+GPIO.setup(A_RED, GPIO.OUT)
+GPIO.setup(A_GREEN, GPIO.OUT)
+
+# CNT 여부 초기에 없다고 가정
+ACNT = False
 
 def register_A(car):
     url = 'http://203.253.128.177:7579/Mobius/sch_platform_4/Car_list/A'
@@ -119,7 +137,7 @@ lists=[]
 
 plt.style.use('dark_background')
 
-img_ori = cv2.imread('A-1.jpeg')
+img_ori = cv2.imread('B-2.jpeg')
 
 
 height, width, channel = img_ori.shape
@@ -430,6 +448,57 @@ plt.show()
 
 car = quote_plus(result_chars)
 
+try : 
+    while True :
+        GPIO.output(A_TRIG, False)
+        time.sleep(0.00001)
+
+        GPIO.output(A_TRIG, True)
+        time.sleep(0.00001)
+        GPIO.output(A_TRIG, False)
+
+        # Echo가 OFF 되는 시점을 시작 시간으로 잡기
+        while GPIO.input(A_ECHO) == 0: 
+            A_START = time.time()
+
+        # Echo가 다시 ON이 되는 시점을 수신시간으로 잡기
+        while GPIO.input(A_ECHO) == 1:     
+            A_STOP = time.time()
+
+        # 초음파가 수신되는 시간으로 거리 계산
+        A_T = A_STOP - A_START
+        A_L = A_T * 17000
+        A_L = round(A_L, 2)
+
+        # 거리 오류
+        if (A_L >= 200 or A_L <= 0) :
+            print("-1")
+
+        # 주차자리 있음 (초록불)
+        elif (A_L > 15 and A_L <= 40) :
+            GPIO.output(A_RED, GPIO.LOW)
+            GPIO.output(A_GREEN, GPIO.HIGH)
+            #print('Distance is ', L, ' cm')
+
+            if ACNT == True :
+                delete_car_A(car)
+                ACNT = False
+
+
+        # 주차자리 없음 (빨간불)
+        elif (A_L <= 15) :
+            GPIO.output(A_RED, GPIO.HIGH)
+            GPIO.output(A_GREEN, GPIO.LOW)
+            #print('Distance is ', L, ' cm')
+
+            if ACNT == False :
+                register_A(car)
+                ACNT = True
+
+        time.sleep(0.1)
+        
+except KeyboardInterrupt:
+    GPIO.cleanup()
 
 # A=False
 # B=False
